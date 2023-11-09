470a686e7ceee65133e21b9cb8e3af0537423f31
Abdullo <148504180+mbovabi@users.noreply.github.com>
2023-11-09 00:38:22 -0600

    Added all

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "aws_key_pair" "class2" {
   key_name   = "class2-key"
   public_key = file("~/.ssh/id_rsa.pub")
 }
+
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = var.key_name
+  public_key = var.public_key
+}
---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "key_name" {
+  description = "The name for the AWS key pair"
+  type        = string
+}
+
+variable "public_key" {
+  description = "The public key material for the AWS key pair"
+  type        = string
+}
